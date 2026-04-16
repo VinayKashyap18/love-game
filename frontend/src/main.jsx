e32b0cb59ea0b0acb1f7b2d4cb6d17353223b46e
VinayKashyap18 <119151715+VinayKashyap18@users.noreply.github.com>
2026-04-16 22:47:06 +0530

    Add files via upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { Buffer } from 'buffer'
-
-// Polyfills for simple-peer
-if (typeof global === 'undefined') {
-  window.global = window;
-}
-if (typeof window.Buffer === 'undefined') {
-  window.Buffer = Buffer;
-}
-
 import App from './App.jsx'
 import './App.css'
 
